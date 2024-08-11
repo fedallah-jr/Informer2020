488ae664531931c4d942e84bfde27d199fec7b9f
fedallah-jr <kilciali2728@gmail.com>
2024-08-12 01:58:52 +0300

    Update data_loader.py

--- data/data_loader.py
+++ data/data_loader.py
@@ -320,59 +320,59 @@ class Dataset_Pred(Dataset):
         self.data_path = data_path
         self.__read_data__()
 
-def __read_data__(self):
-    self.scaler = StandardScaler()
-    df_raw = pd.read_csv(os.path.join(self.root_path, self.data_path))
+    def __read_data__(self):
+        self.scaler = StandardScaler()
+        df_raw = pd.read_csv(os.path.join(self.root_path, self.data_path))
+        
+        if self.cols:
+            cols = self.cols.copy()
+            cols.remove(self.target)
+        else:
+            cols = list(df_raw.columns)
+            cols.remove(self.target)
+            cols.remove('date')
+        
+        df_raw = df_raw[['date'] + cols + [self.target]]
+        
+        border1 = len(df_raw) - self.seq_len
+        border2 = len(df_raw)
+        
+        if self.features == 'M' or self.features == 'MS':
+            cols_data = df_raw.columns[1:]
+            df_data = df_raw[cols_data]
+        elif self.features == 'S':
+            df_data = df_raw[[self.target]]
     
-    if self.cols:
-        cols = self.cols.copy()
-        cols.remove(self.target)
-    else:
-        cols = list(df_raw.columns)
-        cols.remove(self.target)
-        cols.remove('date')
-    
-    df_raw = df_raw[['date'] + cols + [self.target]]
-    
-    border1 = len(df_raw) - self.seq_len
-    border2 = len(df_raw)
-    
-    if self.features == 'M' or self.features == 'MS':
-        cols_data = df_raw.columns[1:]
-        df_data = df_raw[cols_data]
-    elif self.features == 'S':
-        df_data = df_raw[[self.target]]
-
-    if self.scale:
-        self.scaler.fit(df_data.values)
-        data = self.scaler.transform(df_data.values)
-    else:
-        data = df_data.values
-    
-    tmp_stamp = df_raw[['date']][border1:border2]
-    tmp_stamp['date'] = pd.to_datetime(tmp_stamp.date)
-    pred_dates = pd.date_range(tmp_stamp.date.values[-1], periods=self.pred_len + 1, freq=self.freq)
-    
-    df_stamp = pd.DataFrame(columns=['date'])
-    df_stamp.date = list(tmp_stamp.date.values) + list(pred_dates[1:])
-    data_stamp = time_features(df_stamp, timeenc=self.timeenc, freq=self.freq[-1:])
-    
-    self.data_x = data[border1:border2]
-    
-    if self.inverse:
-        self.data_y = df_data.values[border1:border2]
-    else:
-        self.data_y = data[border1:border2]
-    
-    self.data_stamp = data_stamp
-    
-    # Extract the latest Close value before the prediction period in original scale
-    if self.scale:
-        self.latest_close_scaled = data[-1]  # Last data point in the scaled data
-        self.latest_close = df_data.values[-1]  # Last data point in the original data before scaling
-    else:
-        self.latest_close_scaled = None
-        self.latest_close = df_data.values[-1]  # Already in original scale
+        if self.scale:
+            self.scaler.fit(df_data.values)
+            data = self.scaler.transform(df_data.values)
+        else:
+            data = df_data.values
+        
+        tmp_stamp = df_raw[['date']][border1:border2]
+        tmp_stamp['date'] = pd.to_datetime(tmp_stamp.date)
+        pred_dates = pd.date_range(tmp_stamp.date.values[-1], periods=self.pred_len + 1, freq=self.freq)
+        
+        df_stamp = pd.DataFrame(columns=['date'])
+        df_stamp.date = list(tmp_stamp.date.values) + list(pred_dates[1:])
+        data_stamp = time_features(df_stamp, timeenc=self.timeenc, freq=self.freq[-1:])
+        
+        self.data_x = data[border1:border2]
+        
+        if self.inverse:
+            self.data_y = df_data.values[border1:border2]
+        else:
+            self.data_y = data[border1:border2]
+        
+        self.data_stamp = data_stamp
+        
+        # Extract the latest Close value before the prediction period in original scale
+        if self.scale:
+            self.latest_close_scaled = data[-1]  # Last data point in the scaled data
+            self.latest_close = df_data.values[-1]  # Last data point in the original data before scaling
+        else:
+            self.latest_close_scaled = None
+            self.latest_close = df_data.values[-1]  # Already in original scale
 
 def __getitem__(self, index):
     s_begin = index
